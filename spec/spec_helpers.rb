495d80c824d21cb9afa9d6f0078209f0ab54a00f
vgabou <vincent.gabou@sociabliz.com>
2014-11-13 11:53:47 +0100

    [SpecHelpers] Basic Rspec configuration added

--- spec/spec_helpers.rb
+++ spec/spec_helpers.rb
@@ -7,3 +7,16 @@ require File.join(File.dirname(__FILE__), '..', 'lib', 'system')
 def sample_pid_file_path
   File.join(File.dirname(__FILE__), '..', 'spec', 'sample_pid_file.pid')
 end
+
+RSpec.configure do |config|
+  config.mock_with :rspec
+  config.expect_with :rspec
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+  config.color = true
+
+  # Force using expect and not should
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
