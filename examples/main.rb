594d4a4b157c6e53400b1c2a1fba8594230f7c6c
Stéphane Akkaoui <stephane.akkaoui@imeuble.info>
2014-12-22 11:57:05 +0100

    [Examples] Fix sample code to match library features

--- examples/main.rb
+++ examples/main.rb
@@ -1,46 +1,67 @@
 require_relative "../ziltoid"
 
-Default logger is to $stdout, pass in a Logger instance (or Logger child class instance) to override it
-watcher = Ziltoid.watcher.new(
-  :logger => Logger.new(File.new('/tmp/ziltoid.log')),
+# Configure notifiers
+notifiers = [
+  Ziltoid::EmailNotifier.new(
+    :via_options => {
+      :address        => 'smtp.ziltoid.com',
+      :port           => '25',
+      :domain         => "ziltoid.com"
+    },
+    :subject => "[Balloonz] Ziltoid message",
+    :to => ['developers@sociabliz.com'],
+    :from => 'ziltoidd@ziltoid.com'
+  )
+]
+# Create a watcher, with custom file logger and notifiers
+watcher = Ziltoid::Watcher.new(
+  :logger => Logger.new(File.new('/home/ror/site/prod/log/ziltoid.log', 'a+')),
   :progname => "Ziltoid Watcher",
-  :log_level => Logger::DEBUG
+  :log_level => Logger::DEBUG,
+  :notifiers => notifiers
 )
 
-watcher.notifier = EmailNotifier.new(
-  :via_options => {
-    :address        => 'smtp-out.bearstech.com',
-    :port           => '25',
-    :domain         => "banqyou.appliz.com"
-  }
-  :subject => "[#{APP_NAME}] #{self.process.name} - #{transition.to_name.to_s}"
-  :to => ['stephane.akkaoui@sociabliz.com']
-  :from => 'bluepill@epicbattle.appliz.com',
-)
+# add production processes
+production_ports = [4567, 4568]
+production_ports.each do |port|
+  watcher.add(Ziltoid::Process.new("thin - production - #{port}", {
+    :pid_file => "/home/ror/http/tmp/thin.#{port}.pid",
+    :commands => {
+      :start => "/home/ror/gem/bin/thin start -R config.ru -C /home/ror/http/thin_production.yml",
+      :stop => "/home/ror/gem/bin/thin stop -R config.ru -C /home/ror/http/thin_production.yml"
+    },
+    :limit => {
+      :ram => 256,
+      :cpu => 10
+    }
+  }))
+end
 
-watcher.add(Ziltoid::Process.new("thin", {
-  :pid_file => "/tmp/pids/thin.pid",
+# add staging processes
+watcher.add(Ziltoid::Process.new("thin - staging - 4565", {
+  :pid_file => "/home/ror/http/tmp/thin.4565.pid",
   :commands => {
-    :start => "/home/ror/gem/bin/thin start -R config.ru -C /home/ror/http/thin_production.yml",
-    :stop => "/home/ror/gem/bin/thin stop -R config.ru -C /home/ror/http/thin_production.yml"
+    :start => "RAILS_ENV=staging /home/ror/gem/bin/thin start -R config.ru -C /home/ror/http/thin_staging.yml",
+    :stop => "RAILS_ENV=staging /home/ror/gem/bin/thin stop -R config.ru -C /home/ror/http/thin_staging.yml"
   },
-  :max_ram => "256",
-  :max_cpu => "10"
-}))
-watcher.add(Ziltoid::Process.new("Lighty", {
-  :pid_file => "/tmp/pids/lighty.pid",
-  :commands {
-    :start => "/home/ror/gem/bin/lighty start",
-    :stop => "/home/ror/gem/bin/lighty stop"
-  },
-  :max_ram => "256",
-  :max_cpu => "5"
-}))
-watcher.add(Ziltoid::DiskUsage.new("Disk Usage", {
-  :max => "80"
-}))
-watcher.add(Ziltoid::Load.new("Load", {
-  :max => "3"
+  :limit => {
+    :ram => 256,
+    :cpu => 10
+  }
 }))
 
+# add webserver process
+watcher.add(Ziltoid::Process.new("Lighty", {
+  :pid_file => "/home/ror/http/tmp/lighttpd.pid",
+  :commands => {
+    :start => "/home/ror/bin/lighty start",
+    :stop => "/home/ror/bin/lighty stop"
+  },
+  :limit => {
+    :ram => 256,
+    :cpu => 10
+  }
+}))
+
+# Begin the watch
 watcher.watch!
